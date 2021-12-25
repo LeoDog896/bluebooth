3abc1b814a3e3eff7233d6a0d496da9a2163ed43
LeoDog896 <LeoDog896@hotmail.com>
2021-12-25 13:25:44 -0500

    Begin changes

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,76 @@
 extern crate itertools;
+#[macro_use] extern crate prettytable;
 
-use bluer::{Adapter, AdapterEvent, Address, DeviceEvent, AddressType};
+use bluer::{Adapter, AdapterEvent, Address, DeviceEvent};
 use futures::{pin_mut, stream::SelectAll, StreamExt};
 use std::{collections::HashSet, env};
 use tokio::sync::Mutex;
-use cli_table::{Table, print_stdout, WithTitle};
 use std::sync::Arc;
 use std::collections::HashMap;
+use prettytable::{Row, Cell};
 
-#[derive(Table)]
-struct BlueboothDevice {
-    #[table(title = "Address")]
-    address: AddressType,
-    #[table(title = "Name")]
-    name: String,
-    #[table(title = "Icon")]
-    icon: String,
-    #[table(title = "Class")]
-    class: String,
-    #[table(title = "UUID")]
-    uuid: String,
-    #[table(title = "Paired")]
-    paired: bool,
-    #[table(title = "Connected")]
-    connected: bool,
-    #[table(title = "Trusted")]
-    trusted: bool,
-    #[table(title = "Modalias")]
-    modalias: String,
-    #[table(title = "RSSI")]
-    rssi: String,
-    #[table(title = "TX Power")]
-    tx_power: String,
-    #[table(title = "Manufacturer Data")]
-    manufacturer_data: String,
-    #[table(title = "Service Data")]
-    service_data: String
-}
-
-async fn to_blueboot_device(adapter: &Adapter, addr: Address) -> bluer::Result<BlueboothDevice> {
+async fn to_bluetooth_info(adapter: &Adapter, addr: Address) -> bluer::Result<Vec<String>> {
     let device = adapter.device(addr)?;
-    Ok(BlueboothDevice {
-        address: device.address_type().await?,
-        name: device.name().await?.unwrap_or("None".to_string()),
-        icon: device.icon().await?.unwrap_or("None".to_string()),
-        class: device.class().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
-        uuid: device.uuids().await?.map(|it| itertools::join(&it, ", ")).unwrap_or("None".to_string()),
-        paired: device.is_paired().await?,
-        connected: device.is_connected().await?,
-        trusted: device.is_trusted().await?,
-        modalias: device.modalias().await?.map(|it| format!("{:?}", it)).unwrap_or("None".to_string()),
-        rssi: device.rssi().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
-        tx_power: device.tx_power().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
-        manufacturer_data: device.tx_power().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
-        service_data: device.service_data().await?
+    Ok(vec![
+        device.address_type().await?.to_string(),
+        device.name().await?.unwrap_or("None".to_string()),
+        device.icon().await?.unwrap_or("None".to_string()),
+        device.class().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
+        device.uuids().await?.map(|it| itertools::join(&it, ", ")).unwrap_or("None".to_string()),
+        device.is_paired().await?.to_string(),
+        device.is_connected().await?.to_string(),
+        device.is_trusted().await?.to_string(),
+        device.modalias().await?.map(|it| format!("{:?}", it)).unwrap_or("None".to_string()),
+        device.rssi().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
+        device.tx_power().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
+        device.tx_power().await?.map(|it| it.to_string()).unwrap_or("None".to_string()),
+        device.service_data().await?
             .map(|it| it.iter().map(|(k, v)| format!("{}: {}", k, itertools::join(v, ", "))).collect())
             .map(|it: HashSet<String>| itertools::join(&it, ", ")).unwrap_or("None".to_string()),
-    })
+    ])
 }
 
-type SafeBlueboothDeviceMap = Arc<Mutex<HashMap<Address, BlueboothDevice>>>;
+type ThreadSafeBlueboothDeviceMap = Arc<Mutex<HashMap<Address, Vec<String>>>>;
 
-fn print_devices<T: Table>(devices: T) -> std::io::Resuit<()> {
-    print_stdout(devices.with_title())?;
-    println!();
+async fn print_table(devices: ThreadSafeBlueboothDeviceMap) {
+    let editable_devices = devices.lock().await;
+
+    let mut table = table!(
+        ["Address", "Name", "Icon", "Class", "UUIDs", "Paired", "Connected", "Trusted", "Modalias", "RSSI", "Tx Power", "Service Data"]
+    );
+
+    editable_devices.values().for_each(|device| {
+        table.add_row(Row::new(device.iter().map(|it| Cell::new(&it)).collect()));
+    });
+
+    table.printstd();
 }
 
-async fn add_device(address: Address, device: BlueboothDevice, devices: SafeBlueboothDeviceMap) -> std::io::Result<()> {
+async fn set_info(address: Address, device_info: Vec<String>, devices: ThreadSafeBlueboothDeviceMap) -> std::io::Result<()> {
     let mut editable_devices = devices.lock().await;
-    editable_devices.insert(address, device);
+    editable_devices.insert(address, device_info);
 
-
-    print_stdout(editable_devices.values().with_title())?;
-    println!();
+    print_table(devices.clone()).await;
 
     Ok(())
 }
 
-async fn remove_device(address: Address, devices: SafeBlueboothDeviceMap) -> std::io::Result<()> {
+async fn remove_info(address: Address, devices: ThreadSafeBlueboothDeviceMap) -> std::io::Result<()> {
     let mut editable_devices = devices.lock().await;
     editable_devices.remove(&address);
 
-    print_stdout(editable_devices.values().with_title())?;
-    println!();
+    print_table(devices.clone()).await;
 
     Ok(())
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
-    let devices: SafeBlueboothDeviceMap = Arc::new(Mutex::new(HashMap::new()));
+    let devices: ThreadSafeBlueboothDeviceMap = Arc::new(Mutex::new(HashMap::new()));
 
     let with_changes = env::args().any(|arg| arg == "--changes");
     let filter_addr: HashSet<_> = env::args().filter_map(|arg| arg.parse::<Address>().ok()).collect();
 
-    env_logger::init();
     let session = bluer::Session::new().await?;
     let adapter_names = session.adapter_names().await?;
     let adapter_name = adapter_names.first().expect("No Bluetooth adapter present");
@@ -116,10 +92,9 @@ async fn main() -> bluer::Result<()> {
                             continue;
                         }
 
-                        let device = to_blueboot_device(&adapter, addr).await?;
+                        let device = to_bluetooth_info(&adapter, addr).await?;
 
-                        add_device(addr, device, devices.clone()).await?;
-                        
+                        set_info(addr, device, devices.clone()).await?;
 
                         if with_changes {
                             let device = adapter.device(addr)?;
@@ -128,7 +103,7 @@ async fn main() -> bluer::Result<()> {
                         }
                     }
                     AdapterEvent::DeviceRemoved(addr) => {
-                        remove_device(addr, devices.clone()).await?;
+                        remove_info(addr, devices.clone()).await?;
                     }
                     _ => (),
                 }
